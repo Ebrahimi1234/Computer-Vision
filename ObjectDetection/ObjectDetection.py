6177ff07438186cae33fbc4b63ee558cb8e7f68f
Reza Ebrahimi <35271440+Ebrahimi1234@users.noreply.github.com>
2023-03-31 07:33:22 +0430

    Add files via upload

---
+++ ObjectDetection/ObjectDetection.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 12:11:06 2023
+
+@author: Reza Ebrahimi
+"""
+
+import cv2
+
+#img = cv2.imread('lena.png')
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+classNames=[]
+classfile ='coco.names'
+thres = 0.5
+with open(classfile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+    
+print(classNames)
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)
+
+while True:
+    success,img  = cap.read()
+    classIds, confs, bbox= net.detect(img,confThreshold=thres)
+    print(classIds,bbox)
+
+    if len(classIds) != 0:
+
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img,box, color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10, box[1]+20),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),1)
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200, box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),1)
+            
+    cv2.imshow("output", img)
+    cv2.waitKey(1)
